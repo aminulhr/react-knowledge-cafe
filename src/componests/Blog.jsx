92c9fd7ccb91e0c5f23a6b3ca9bb884b4560b879
Aminul Haque Rana <aminulhaquerana1@gmail.com>
2025-01-07 14:17:06 +0600

    bookmark icon add

--- src/componests/Blog.jsx
+++ src/componests/Blog.jsx
@@ -1,16 +1,45 @@
+import { faBookmark } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "../Blog.css";
+
 const Blog = ({ blog }) => {
+  const { hashtags } = blog;
   return (
     <div>
       <div className="m-5 p-3">
         <img className="cover" src={blog.cover} alt="" srcSet="" />
-        <div className="display flex">
-          <img className="author-img" src={blog.author_img} alt="" srcSet="" />
-          <h1 className="text-center">{blog.author}</h1>
+        <div className=" flex justify-between ">
+          <div className=" flex ">
+            <img
+              className="author-img"
+              src={blog.author_img}
+              alt=""
+              srcSet=""
+            />
+            <div className="flex flex-col text-black justify-center">
+              <h1 className="text-center">{blog.author}</h1>
+              <h2>{blog.posted_date}</h2>
+            </div>
+          </div>
+          <div className="flex items-center ">
+            <p>{blog.reading_time} min read</p>
+            <button>
+              <FontAwesomeIcon
+                className="text-black p-2 text-xl"
+                icon={faBookmark}
+              />
+            </button>
+          </div>
         </div>
-        <p className="text-4xl">{blog.title}</p>
-        <p>{blog.hashtags}</p>
-        <button className="text-xl text-blue-600 h-1">Mark as Read</button>
+        <h1 className="text-4xl">{blog.title}</h1>
+        <p>
+          {hashtags.map((hash, index) => (
+            <span key={index} className="text-blue-500 p-1">
+              <a href="">{hash}</a>
+            </span>
+          ))}
+        </p>
+        <button>Mark as Read</button>
       </div>
     </div>
   );
--- src/componests/Header.jsx
+++ src/componests/Header.jsx
@@ -1,51 +1,8 @@
 const Header = () => {
   return (
-    <div>
-      <div className="navbar bg-gray-200">
-        <div className="flex-1">
-          <a className="btn btn-ghost text-black text-2xl  ">Knowledge Cafe</a>
-        </div>
-        <div className="flex-none gap-2">
-          <div className="form-control">
-            <input
-              type="text"
-              placeholder="Search"
-              className="input input-bordered w-24 md:w-auto"
-            />
-          </div>
-          <div className="dropdown dropdown-end">
-            <div
-              tabIndex={0}
-              role="button"
-              className="btn btn-ghost btn-circle avatar"
-            >
-              <div className="w-10 rounded-full">
-                <img
-                  alt="Tailwind CSS Navbar component"
-                  src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
-                />
-              </div>
-            </div>
-            <ul
-              tabIndex={0}
-              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
-            >
-              <li>
-                <a className="justify-between">
-                  Profile
-                  <span className="badge">New</span>
-                </a>
-              </li>
-              <li>
-                <a>Settings</a>
-              </li>
-              <li>
-                <a>Logout</a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </div>
+    <div className="flex justify-between items-center p-5">
+      <h1 className="text-4xl text-black">Knowlag Cafa</h1>
+      <img src="/src/images/profile.png" alt="" srcSet="" />
     </div>
   );
 };
